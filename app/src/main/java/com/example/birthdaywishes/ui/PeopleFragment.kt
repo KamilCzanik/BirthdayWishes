706fce2f175c36da52e221bb82ea24373e606552
kamil <kamil@czanik.xyz>
2019-05-30 20:47:34 +0200

    added peopleFragment

---
+++ app/src/main/java/com/example/birthdaywishes/ui/PeopleFragment.kt
@@ -0,0 +1,36 @@
+package com.example.birthdaywishes.ui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.birthdaywishes.R
+import com.example.birthdaywishes.di.dao.DaoModule
+import com.example.birthdaywishes.di.people.DaggerPeopleComponent
+import com.example.birthdaywishes.di.people.PeopleModule
+import com.example.birthdaywishes.viewmodel.PeopleViewModel
+import javax.inject.Inject
+
+
+class PeopleFragment : Fragment() {
+
+    @Inject lateinit var viewModel: PeopleViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        injectDependencies()
+        return inflater.inflate(R.layout.fragment_people, container, false)
+    }
+
+    private fun injectDependencies() {
+        DaggerPeopleComponent
+            .builder()
+            .peopleModule(PeopleModule(activity!!.application))
+            .daoModule(DaoModule(activity!!.application))
+            .build()
+            .inject(this)
+    }
+}
