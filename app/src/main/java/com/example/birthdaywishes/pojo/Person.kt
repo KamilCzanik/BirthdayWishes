ffb092cea249666a4136d99dbd7801e3aec82337
kamil <kamil@czanik.xyz>
2019-06-01 20:39:25 +0200

    implemented PeopleAdapter

--- app/src/main/java/com/example/birthdaywishes/pojo/Person.kt
+++ app/src/main/java/com/example/birthdaywishes/pojo/Person.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "person_table")
-class Person(val name: String, val birthday: Birthday, val phoneNumber: String){
+data class Person(val name: String, val birthday: Birthday, val phoneNumber: String){
 
     @PrimaryKey(autoGenerate = true) var id: Int? = null
 
--- app/src/main/java/com/example/birthdaywishes/ui/PeopleAdapter.kt
+++ app/src/main/java/com/example/birthdaywishes/ui/PeopleAdapter.kt
@@ -1,20 +1,32 @@
 package com.example.birthdaywishes.ui
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.birthdaywishes.databinding.PersonItemBinding
 import com.example.birthdaywishes.pojo.Person
 
-class PeopleAdapter(private val people: List<Person>) : RecyclerView.Adapter<PeopleAdapter.ViewHolder>() {
+class PeopleAdapter : ListAdapter<Person,PeopleAdapter.ViewHolder>(DIFF_CALLBACK) {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         ViewHolder(PersonItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
 
-    override fun getItemCount() = people.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(getItem(position))
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(people[position])
 
+    companion object {
+
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Person>() {
+
+            override fun areItemsTheSame(oldItem: Person, newItem: Person) = oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Person, newItem: Person) = oldItem == newItem
+        }
+    }
 
     class ViewHolder(private val binding: PersonItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
