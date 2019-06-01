9f037491419048bfba21870478b39fc5b10bdad7
kamil <kamil@czanik.xyz>
2019-06-01 20:27:14 +0200

    added peopleViewmodel interface

--- app/src/main/java/com/example/birthdaywishes/ui/PeopleFragment.kt
+++ app/src/main/java/com/example/birthdaywishes/ui/PeopleFragment.kt
@@ -6,18 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
 
 import com.example.birthdaywishes.R
 import com.example.birthdaywishes.di.dao.DaoModule
 import com.example.birthdaywishes.di.people.DaggerPeopleComponent
 import com.example.birthdaywishes.di.people.PeopleModule
+import com.example.birthdaywishes.pojo.Person
 import com.example.birthdaywishes.viewmodel.PeopleViewModel
 import javax.inject.Inject
 
 
 class PeopleFragment : Fragment() {
 
-    @Inject lateinit var viewModel: PeopleViewModel
+    @Inject lateinit var viewModel: ViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
@@ -32,5 +34,9 @@ class PeopleFragment : Fragment() {
             .daoModule(DaoModule(activity!!.application))
             .build()
             .inject(this)
+    }
+
+    interface ViewModel {
+        val people: LiveData<List<Person>>
     }
 }
--- app/src/main/java/com/example/birthdaywishes/viewmodel/PeopleViewModel.kt
+++ app/src/main/java/com/example/birthdaywishes/viewmodel/PeopleViewModel.kt
@@ -4,12 +4,13 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.example.birthdaywishes.pojo.Person
 import com.example.birthdaywishes.repository.PeopleRepository
+import com.example.birthdaywishes.ui.PeopleFragment
 import javax.inject.Inject
 
-class PeopleViewModel(application: Application) : AndroidViewModel(application) {
+class PeopleViewModel(application: Application) : AndroidViewModel(application),PeopleFragment.ViewModel {
 
     @Inject lateinit var repository: PeopleRepository
-    val people by lazy { repository.people }
+    override val people by lazy { repository.people }
 
     fun delete(person: Person) { repository.delete(person) }
 }
