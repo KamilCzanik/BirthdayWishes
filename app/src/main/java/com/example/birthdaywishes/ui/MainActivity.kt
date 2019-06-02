15a0414c5f9e33da2da75a414b1c4a660ee96fba
kamil <kamil@czanik.xyz>
2019-06-02 19:25:07 +0200

    moved MainActivity to ui package

--- app/src/main/java/com/example/birthdaywishes/ui/MainActivity.kt
+++ app/src/main/java/com/example/birthdaywishes/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.birthdaywishes
+package com.example.birthdaywishes.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.birthdaywishes.R
 
 class MainActivity : AppCompatActivity() {
 
