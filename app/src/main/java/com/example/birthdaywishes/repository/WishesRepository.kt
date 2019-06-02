de68fdb3a8c453f7057966c687dcb412fc8f7d2a
kamil <kamil@czanik.xyz>
2019-06-02 17:36:21 +0200

    implemented wishesRepository

---
+++ app/src/main/java/com/example/birthdaywishes/repository/WishesRepository.kt
@@ -0,0 +1,33 @@
+package com.example.birthdaywishes.repository
+
+import android.os.AsyncTask
+import androidx.lifecycle.LiveData
+import com.example.birthdaywishes.pojo.Wishes
+import com.example.birthdaywishes.repository.db.WishesDao
+import javax.inject.Inject
+
+class WishesRepository @Inject constructor(private val wishesDao: WishesDao) {
+
+    val wishes: LiveData<List<Wishes>> = wishesDao.getAllWishes()
+
+    fun delete(wishes: Wishes) {
+        DeleteWishesAsyncTask(wishesDao).doInBackground(wishes)
+    }
+
+    fun add(wishes: Wishes) {
+        InsertWishesAsyncTask(wishesDao).doInBackground(wishes)
+    }
+
+    private class InsertWishesAsyncTask(private val wishesDao: WishesDao) : AsyncTask<Wishes, Any, Any>() {
+
+        public override fun doInBackground(vararg wishesArgs: Wishes) { wishesArgs.forEach { wishesDao.insert(it) } }
+    }
+
+    private class DeleteWishesAsyncTask(private val wishesDao: WishesDao) : AsyncTask<Wishes, Any, Any>() {
+
+        public override fun doInBackground(vararg params: Wishes) {
+            params.forEach { wishesDao.delete(it) }
+        }
+
+    }
+}
