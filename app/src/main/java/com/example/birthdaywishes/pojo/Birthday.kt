efa1f3112222eae1b01747a3614ea6651f93bed2
kamil <kamil@czanik.xyz>
2019-06-03 14:04:00 +0200

    formatted code

--- app/src/main/java/com/example/birthdaywishes/pojo/Birthday.kt
+++ app/src/main/java/com/example/birthdaywishes/pojo/Birthday.kt
@@ -6,14 +6,10 @@ data class Birthday constructor(val day: Int,val month: Int) {
 
     companion object {
         fun isDateValid(day: Int,month: Int) = day in 1..DaysOfMonth[month] && month in 1..12
-        fun of(day: Int,month: Int) = if(isDateValid(
-                day,
-                month
-            )
-        ) Birthday(day, month) else Birthday(1, 1)
+        fun of(day: Int,month: Int) = if(isDateValid(day, month)) Birthday(day, month) else Birthday(1, 1)
     }
 
     fun toJsonString() = "{\"day\":$day,\"month\":$month}"
 
-    fun toDateString() = "$day + ${DateFormatSymbols().months[month]}"
+    fun toDateString() = "$day  ${DateFormatSymbols().months[month-1]}"
 }
