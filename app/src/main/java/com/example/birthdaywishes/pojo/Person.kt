a880f8a1142ad41b2dfe78ed964fe9727f930d6f
kamil <kamil@czanik.xyz>
2019-06-03 10:45:49 +0200

    person implements serializable

--- app/src/main/java/com/example/birthdaywishes/pojo/Person.kt
+++ app/src/main/java/com/example/birthdaywishes/pojo/Person.kt
@@ -2,10 +2,11 @@ package com.example.birthdaywishes.pojo
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 
 @Entity(tableName = "person_table")
-data class Person(val name: String, val birthday: Birthday, val phoneNumber: String){
+data class Person(val name: String, val birthday: Birthday, val phoneNumber: String) : Serializable{
 
     @PrimaryKey(autoGenerate = true) var id: Int? = null
 
