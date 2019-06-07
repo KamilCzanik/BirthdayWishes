72240172994d64c582054ba2bac21323cb7f6675
kamil <kamil@czanik.xyz>
2019-06-07 18:27:15 +0200

    Added EmptyWishes class

--- app/src/main/java/com/example/birthdaywishes/pojo/Wishes.kt
+++ app/src/main/java/com/example/birthdaywishes/pojo/Wishes.kt
@@ -4,6 +4,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "wishes_table")
-data class Wishes(val content: String) {
+open class Wishes(val content: String) {
     @PrimaryKey var id: Int? = null
+
+    override fun equals(other: Any?): Boolean {
+        return content.contentEquals((other as Wishes).content)
+    }
 }
+
+class EmptyWishes : Wishes("")
