4481410b9a82b2b839eab2696a6a6ec2b435e308
kamil <kamil@czanik.xyz>
2019-06-07 17:56:11 +0200

    fixed logic

--- app/src/main/java/com/example/birthdaywishes/util.kt
+++ app/src/main/java/com/example/birthdaywishes/util.kt
@@ -1,28 +1,18 @@
 package com.example.birthdaywishes
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.ViewPropertyAnimator
 
 fun View.slideDownAndDisappear() {
     animate()
-        .translationX(0f)
-        .alpha(0f)
-        .setListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationEnd(animation: Animator?) {
-                super.onAnimationEnd(animation)
-                visibility = GONE
-            }
-        })
+        .translationY(height.toFloat())
+        .withEndAction { visibility = GONE }
 }
 
-fun View.appearAndSlideUp() {
+fun View.appearAndSlideUp(): ViewPropertyAnimator {
     visibility = VISIBLE
-    alpha = 0f
-    animate()
-        .translationY(height.toFloat())
-        .alpha(1.0f)
-        .setListener(null)
+    return animate()
+        .translationY(0.0f)
 }
