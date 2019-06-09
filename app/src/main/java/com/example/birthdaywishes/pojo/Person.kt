fd954b26db6e87fc3a8fdb8d2c5230c66ba145d2
kamil <kamil@czanik.xyz>
2019-06-09 23:05:21 +0200

    fixed missing id bug

--- app/src/main/java/com/example/birthdaywishes/pojo/Person.kt
+++ app/src/main/java/com/example/birthdaywishes/pojo/Person.kt
@@ -6,9 +6,10 @@ import java.io.Serializable
 
 
 @Entity(tableName = "person_table")
-data class Person(val name: String, val birthday: Birthday, val phoneNumber: String) : Serializable{
-
-    @PrimaryKey(autoGenerate = true) var id: Long? = null
+data class Person(
+    val name: String,
+    val birthday: Birthday, val phoneNumber: String,
+    @PrimaryKey(autoGenerate = true) var id: Long = 0) : Serializable{
 
     fun isDataValid() = name.isNotEmpty() &&
             phoneNumber.isNotEmpty() &&
