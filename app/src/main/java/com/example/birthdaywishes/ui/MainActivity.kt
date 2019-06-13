2b256888f2238b0b6ebc50c9c50f9e23f335f2cc
kamil <kamil@czanik.xyz>
2019-06-13 12:05:26 +0200

    added default wishes

--- app/src/main/java/com/example/birthdaywishes/ui/MainActivity.kt
+++ app/src/main/java/com/example/birthdaywishes/ui/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.birthdaywishes.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.birthdaywishes.R
+import com.example.birthdaywishes.pojo.Wishes
+import com.example.birthdaywishes.repository.WishesRepository
+import com.example.birthdaywishes.repository.db.BirthdayWishesDatabase
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(mainActivity_toolbar)
+        importDefaultWishes()
+    }
+
+    private fun importDefaultWishes() {
+        val prefs = getSharedPreferences("com.example.birthdaywishes.ui.MainActivity",MODE_PRIVATE)
+        if (prefs.getBoolean("first_run", true)) {
+            insertWishes()
+            prefs.edit().putBoolean("first_run", false).commit()
+        }
+    }
+
+    private fun insertWishes() {
+        val wishesContents = resources.getStringArray(R.array.wishes_samples)
+        val wishesDao = BirthdayWishesDatabase.getInstance(this).wishesDao()
+        val repository = WishesRepository(wishesDao)
+
+        wishesContents.forEach { content -> repository.add(Wishes(content)) }
     }
 }
