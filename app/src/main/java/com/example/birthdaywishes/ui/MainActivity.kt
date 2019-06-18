93e11c0e8ba298be958ae5a7f73ccd1a722fe273
kamil <kamil@czanik.xyz>
2019-06-18 21:04:16 +0200

    extracted prefs key to const val

--- app/src/main/java/com/example/birthdaywishes/ui/MainActivity.kt
+++ app/src/main/java/com/example/birthdaywishes/ui/MainActivity.kt
@@ -11,6 +11,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val FIRST_RUN_PREFS = "first_run"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,9 +24,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun importDefaultWishes() {
         val prefs = getSharedPreferences("com.example.birthdaywishes.ui.MainActivity",MODE_PRIVATE)
-        if (prefs.getBoolean("first_run", true)) {
+        if (prefs.getBoolean(FIRST_RUN_PREFS, true)) {
             insertWishes()
-            prefs.edit().putBoolean("first_run", false).commit()
+            prefs.edit().putBoolean(FIRST_RUN_PREFS, false).apply()
         }
     }
 
